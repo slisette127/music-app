63ba5b0159b414599fb92c524855ddd3d3be8df1
unknown <slisette127@gmail.com>
2021-09-28 20:53:06 -0500

    styling

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,21 +1,32 @@
 import * as React from 'react';
-import { Card, CardActions, CardContent, Switch, Slider } from '@material-ui/core';
+import { Card, CardActions, CardContent, Switch, Slider, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';
 
 function valuetext(value) {
     return `${value}`;
   }
 
 export default function Dashboard(props) {
+
+    const [volume, setVolume] = React.useState('');
+    const handleChange = (event) => {
+        setVolume(event.target.value);
+      };
+
   return (
 
     <div>
-        <h1>{props.user.userName}</h1>
+        <h1>Welcome {props.user.userName}</h1>
       <div style={{
           display:"flex",
-          margin: "10px"
+          margin: "10px",
           
         }}>
-    <Card sx={{ minWidth: 275 }}>
+    <Card style={{ minWidth: 275,
+                    maxWidth:350,
+                    padding: "15px",
+                    margin: "10px",
+        
+                    }}>
       <CardContent>
        <h1>Online Mode</h1>
         <p>Is this application connected to the internet.</p>
@@ -26,14 +37,18 @@ export default function Dashboard(props) {
       </CardActions>
     </Card>
 
-    <Card sx={{ minWidth: 275 }}>
+    <Card style={{ minWidth: 275,
+                    maxWidth:350,
+                    padding: "15px",
+                    margin: "10px",
+        
+                }}>
       <CardContent>
        <h1>Master Volume</h1>
-        <p>Is this application connected to the internet.</p>
+        <p>Overrides all other sound settings in this application.</p>
       </CardContent>
       <CardActions>
       <Slider
-        aria-label="Temperature"
         defaultValue={30}
         getAriaValueText={valuetext}
         valueLabelDisplay="auto"
@@ -45,11 +60,30 @@ export default function Dashboard(props) {
       </CardActions>
     </Card>
 
-    <Card sx={{ minWidth: 275 }}>
+    <Card style={{ minWidth: 275,
+                    maxWidth:350,
+                    padding: "15px",
+                    margin: "10px",
+        
+                }}>
       <CardContent>
-       <h1>Online Mode</h1>
-        <p>Is this application connected to the internet.</p>
+       <h1>Sound Quality</h1>
+        <p>Manually control the music quality in event of poor connection.</p>
       </CardContent>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label"></InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={volume}
+          label="Volume"
+          onChange={handleChange}
+        >
+          <MenuItem value={1}>Low</MenuItem>
+          <MenuItem value={2}>Normal</MenuItem>
+          <MenuItem value={3}>High</MenuItem>
+        </Select>
+      </FormControl>
       <CardActions>
       
       </CardActions>
